6a2aeff93eb0d40a449ba54d13a6df1bcddcd960
VINOD <69414139+vinodkc123@users.noreply.github.com>
2020-09-21 17:06:34 +0530

    Add files via upload

---
+++ horoGitHub.py
@@ -0,0 +1,56 @@
+
+print ("""
+      0  EXIT
+      1. Aries, the Ram   challenging, impulsive
+      2. Taurus, the Bull   conservative, concrete
+      3. Gemini, the Twins   communicative, playful
+      4. Cancer, the Crab   caring, sensitive
+      5. Leo, the Lion   dominant, proud
+      6. Virgo, the Virgin   careful, minute
+      7. Libra, the Scales   negotiating, ethical
+      8. Scorpio, the Scorpion   hidden, passionate
+      9. Sagittarius, the Archer   impatient, independent
+      10 Capricorn, the Goat   ambitious, decisive
+      11 Aquarius, the Water Bearer   profound, original
+      12 Pisces, the Fish   searching, submissive
+
+
+      """)
+       
+s=input ("enter your Horoscope sign ")
+
+
+
+
+
+if s=="0":
+            print("exiting.......")
+            exit()
+elif s=="1":   
+      print("Aries,You are the  Ram   challenging, impulsive")
+elif s=="2":
+      print("Taurus,You are the   Bull   conservative, concrete ")      
+elif s=="3":
+      print("Gemini, You are the    Twins   communicative, playful   ")
+elif s=="4":
+      print("Cancer, you are the    Crab   caring, sensitive   ")
+elif s=="5":
+      print("Leo, you are the  Lion   dominant, proud  ")
+elif s=="6":
+      print("Virgo,you are the   Virgin   careful, minute ")
+elif s=="7":
+      print("Libra, you are the  Scales   negotiating, ethical)  ")
+elif s=="8":
+      print("Scorpio, you are the  hidden, passionate)  ")            
+elif s=="9":
+      print("Sagittarius,you are the Archer   impatient, independent")
+elif s=="10":
+      print("Capricorn, you are the Goat   ambitious, decisive" )
+elif s=="11":
+      print("Aquarius, you are the Water Bearer   profound, original")
+elif s=="12":
+      print("Pisces, you are the Fish   searching, submissive")
+              
+else:
+ print("Wrong choice.")
+     
